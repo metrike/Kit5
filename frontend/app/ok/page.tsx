93263b850dcb94fb35c324101c95070d10187e8d
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-10-14 16:15:16 +0200

    ok

--- frontend/app/ok/page.tsx
+++ frontend/app/ok/page.tsx
@@ -1,5 +1,6 @@
 import {Button} from "@/components/ui/button";
+import {hello} from "@/ok";
 
 export default function Ok() {
-    return <Button variant="default">Hello</Button>
+    return <Button variant="default">{hello()}</Button>
 }
