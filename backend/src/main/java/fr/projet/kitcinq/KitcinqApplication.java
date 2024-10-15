be778ed71cfbdaa60511436798f2a70613cc5d3f
antomth971 <anthonymathieu21@live.fr>
2024-10-15 14:52:16 +0200

    stash

--- backend/src/main/java/fr/projet/kitcinq/KitcinqApplication.java
+++ backend/src/main/java/fr/projet/kitcinq/KitcinqApplication.java
@@ -2,10 +2,8 @@ package fr.projet.kitcinq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-@EnableSwagger2
 public class KitcinqApplication {
 
 	public static void main(String[] args) {
