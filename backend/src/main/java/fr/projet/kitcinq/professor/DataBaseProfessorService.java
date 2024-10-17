2d6ca88f76c85f915880848a0c675aa4cf8a6163
Livio Tinnirello <tinnirellolivio@gmail.com>
2024-10-17 10:31:54 +0200

    Add student

--- backend/src/main/java/fr/projet/kitcinq/professor/DataBaseProfessorService.java
+++ backend/src/main/java/fr/projet/kitcinq/professor/DataBaseProfessorService.java
@@ -1,7 +1,9 @@
 package fr.projet.kitcinq.professor;
 
 import fr.projet.kitcinq.model.ProfessorEntity;
+import org.springframework.stereotype.Service;
 
+@Service
 public class DataBaseProfessorService implements ProfessorService {
     
     private final ProfessorRepository professorRepository;
